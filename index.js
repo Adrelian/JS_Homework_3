55f0649bc4e4caa823c40f54a335fc57ae4a4628
Anatolii <feelmysmell@yandex.ru>
2023-02-06 23:05:25 +0500

    Задачу с зарплатой не доделал

---
+++ index.js
@@ -0,0 +1,68 @@
+// Домашняя работа
+
+// Задача 1.
+
+// let userNumber = parseInt(prompt("Введите число"))
+
+// function cubeNumber(num) {
+//     result = Math.pow(num, 3)
+//     return console.log(`${num}^3=${result}`)
+// }
+
+// cubeNumber(userNumber)
+
+// Задача 2
+
+function salary() {
+    let userSalary = parseInt(prompt("Введите вашу зарплату"))
+    if (typeof(userSalary) === Number){
+        let result = salary * 0.13 + salary
+        return console.log(`Ваша зарплата после вычета налога равна ${result}`)
+    }
+    return console.log(`Ошибка ${typeof(userSalary)}`)
+}
+
+salary()
+
+// Задача 3
+
+// let num1 = parseInt(prompt("Введите первое число"))
+// let num2 = parseInt(prompt("Введите второе число"))
+// let num3 = parseInt(prompt("Введите третье число"))
+
+// function compareNumber(num1, num2, num3) {
+//     let max = Math.max(num1, num2, num3)
+//     console.log(`Большее число ${max}`)
+// }
+
+// compareNumber(num1, num2, num3)
+
+// Задача 4
+
+// let num1 = parseInt(prompt("Введите первое число"))
+// let num2 = parseInt(prompt("Введите второе число"))
+
+// function sumTwoNumber(num1, num2) {
+//     let sum = num1 + num2
+//     return console.log(`Сумма чисел ${num1} и ${num2} равна ${sum}`)
+// }
+
+// function subtractionTwoNumber(num1, num2) {
+//     let subtraction = (num1 < num2) ? num2 - num1 : num1 - num2
+//     return console.log(`Разница чисел ${num1} и ${num2} равна ${subtraction}`)
+// }
+
+// function divisionTwoNumber(num1, num2) {
+//     let division = num1 / num2
+//     return console.log(`Частное чисел ${num1} и ${num2} равна ${division}`)
+// }
+
+// function multiplicationTwoNumber(num1, num2) {
+//     let multiplication = num1 * num2
+//     return console.log(`Умножение чисел ${num1} и ${num2} равна ${multiplication}`)
+// }
+
+// sumTwoNumber(num1, num2)
+// subtractionTwoNumber(num1, num2)
+// divisionTwoNumber(num1, num2)
+// multiplicationTwoNumber(num1, num2)
